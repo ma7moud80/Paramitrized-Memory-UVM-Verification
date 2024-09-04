c22702fe0f526539c66ddfef436e500ff8dcc1be
mahmoud abdelhalim <67705924+ma7moud80@users.noreply.github.com>
2024-09-04 22:33:35 +0300

    Add files via upload

---
+++ uvm_files_run.bat
@@ -0,0 +1,16 @@
+@echo off
+vlog config.sv
+vlog memory.sv
+vlog data_if.sv
+vlog seq_item.sv
+vlog driver.sv
+vlog monitor.sv
+vlog agent.sv
+vlog scoreboard.sv
+vlog coverage.sv
+vlog env.sv
+vlog sequence1.sv
+vlog mem_test.sv
+vlog test1.sv
+vlog testbench_top.sv
+pause
